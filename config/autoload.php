6789b908ad3a6703a48192bdf5add6f8b25d5d1b
Marko Milenković <milenkovicmarko997@gmail.com>
2021-06-05 16:16:01 +0200

    Create entities, base controller, config autoload

--- config/autoload.php
+++ config/autoload.php
@@ -1,1 +1,13 @@
 <?php
+
+spl_autoload_register(function ($className) {
+    if (file_exists('./Service/' . $className . '.php')) {
+        require_once './Service/' . $className . '.php';
+    } elseif (file_exists('./Controller/' . $className . '.php')) {
+        require_once './Controller/' . $className . '.php';
+    } elseif (file_exists('./Template/' . $className . '.php')) {
+        require_once './Template/' . $className . '.php';
+    } elseif (file_exists('./Entity/' . $className . '.php')) {
+        require_once './Entity/' . $className . '.php';
+    }
+});
